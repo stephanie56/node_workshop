b08e9cd1370c1501c08a38ebc44794b33bd858b8
Stephanie Zeng <stephaniezeng521@gmail.com>
2017-06-17 14:14:05 -0400

    Added regex

--- server.js
+++ server.js
@@ -7,6 +7,14 @@ var fs = require('fs');
 // create a new server instance
 var server = http.createServer();
 
+var re = /^\/currencies\/(\w+)/
+
+var currencies = {
+  'CAD': 1.3,
+  'GBP': 0.78,
+  'USD': 1
+}
+
 // listening to some events: e.g. if open a new browser will return a client obj
 // the server is an instance of eventemiter, meaning it has an on method for listening for events
 server.on('request', function(req, res){
@@ -21,17 +29,31 @@ server.on('request', function(req, res){
     console.log(err);
   });
 
-  // read file system to find index.html - write to response with the contents
-  fs.readFile('index.html', function(err, contents){
-    if (req.url === '/home'){
-      res.write(contents);
-      res.end();
-    }
-    else {
-      res.write('404 not found');
-      res.end();
-    }
-  });
+  // to see if url matches re
+  var match = req.url.match(re);
+
+  if (match) {
+    console.log(match);
+    var value = currencies[match[1]];
+    res.write(`the value is ${value}`);
+    res.end();
+  }
+  else {
+    // read file system to find index.html - write to response with the contents
+    fs.readFile('index.html', function(err, contents){
+      if (req.url === '/home'){
+        res.write(contents);
+        res.end();
+      }
+      else {
+        res.statusCode = 404;
+        res.write('404 not found');
+        res.end();
+      }
+    });
+  }
+
+
 
 });
 
